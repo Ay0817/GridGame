1e7c1afab8f9cfb5776361ca819451d5f1e3217d
Ay <inuhama1543@gmail.com>
2026-03-24 17:51:07 +0900

    add: Audio が再生時に参照するクラス

---
+++ GridGame/src/Audio/SoundData.cpp
@@ -0,0 +1,21 @@
+#include <DxLib.h>
+
+#include "../../include/SoundData.hpp"
+
+SoundData::SoundData(std::string_view name, std::string_view path)
+	: Asset(std::string(name))
+{
+	if (_handle == -1) {
+		_handle = LoadSoundMem(path.data());
+	}
+}
+
+SoundData::~SoundData() {
+	Release();
+}
+
+void SoundData::Release() {
+	if (_handle != -1) {
+		DeleteSoundMem(_handle);
+	}
+}
