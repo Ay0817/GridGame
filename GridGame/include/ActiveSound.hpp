3c316e105a0d2bff22de6a7d9412f65b7e63ba68
Ay <inuhama1543@gmail.com>
2026-03-24 17:47:22 +0900

    add: Audioが再生されるときに内部的に処理を行うクラス

---
+++ GridGame/include/ActiveSound.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+/// @brief 再生されるサウンド
+class ActiveSound {
+private:
+	int _handle; // ハンドル
+public:
+	/// @brief コンストラクタ
+	/// @param handle ハンドル
+	ActiveSound(int handle);
+
+	/// @brief 再生を停止
+	void Stop() const;
+
+	/// @brief 再生中か？
+	/// @return 再生状態
+	bool IsPlaying() const;
+
+	/// @brief ハンドルを取得
+	/// @return ハンドル
+	int GetHandle() const;
+};
