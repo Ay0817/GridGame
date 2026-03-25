4845cd65259230e8c3e6067a55ab8420d23f0f4b
Ay <inuhama1543@gmail.com>
2026-03-25 20:44:32 +0900

    add: テキストを描画する機能を追加

---
+++ GridGame/include/Text.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+#include "Component.hpp"
+#include "Transform.hpp"
+#include "Font.hpp"
+#include "Color.hpp"
+
+/// @brief 
+enum class TextAlign {
+	Left,
+	Center,
+	Right
+};
+
+/// @brief テキストを描画する際に必要な情報を持つ
+struct TextInfo {
+	std::string text;
+	std::shared_ptr<Font> font;
+	TextAlign textAlign;
+	Color color;
+};
+
+/// @brief テキストを表示
+class Text final : public Component {
+private:
+	TextInfo _textInfo; // テキスト情報
+	Transform* _transformCached = nullptr; // Transform cache
+private:
+	int GetAlignedX(int baseX, int textWidth) const;
+public:
+	/// @brief コンストラクタ
+	/// @param textInfo テキスト情報
+	Text(const TextInfo& textInfo);
+
+	/// @brief 初期化処理
+	virtual void Begin() override;
+
+	/// @brief 描画処理
+	virtual void Draw() const override;
+};
---
+++ GridGame/src/Core/Text.cpp
@@ -0,0 +1,41 @@
+#include <DxLib.h>
+
+#include "../../include/Text.hpp"
+#include "../../include/GameObject.hpp"
+
+Text::Text(const TextInfo& textInfo)
+	: Component()
+	, _textInfo(textInfo)
+{}
+
+int Text::GetAlignedX(int baseX, int textWidth) const {
+    switch (_textInfo.textAlign) {
+    case TextAlign::Left:
+        return baseX;
+
+    case TextAlign::Center:
+        return baseX - textWidth / 2;
+
+    case TextAlign::Right:
+        return baseX - textWidth;
+
+    default:
+        return baseX;
+    }
+}
+
+void Text::Begin() {
+    _transformCached = &GetOwner()->GetTransform();
+}
+
+void Text::Draw() const {
+    auto transform = _transformCached;
+    auto handle = _textInfo.font->GetHandle();
+
+    auto w = GetDrawStringWidthToHandle(_textInfo.text.data(), static_cast<int>(_textInfo.text.size()), handle);
+
+    auto x = GetAlignedX(static_cast<int>(transform->position.x), w);
+    auto y = static_cast<int>(transform->position.y);
+
+    DrawStringToHandle(x, y, _textInfo.text.data(), _textInfo.color.ToUInt(), handle);
+}
