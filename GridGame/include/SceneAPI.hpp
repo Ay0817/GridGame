3ecc07462baa0246c19fc9648b3d231f96de428e
Ay <inuhama1543@gmail.com>
2026-03-30 00:12:09 +0900

    update: フェード処理とシーン切り替えを行える関数を追加

--- GridGame/include/SceneAPI.hpp
+++ GridGame/include/SceneAPI.hpp
@@ -5,6 +5,7 @@
 
 #include "SceneManager.hpp"
 #include "Scene.hpp"
+#include "TransitionManager.hpp"
 
 namespace SceneAPI {
 	/// @brief シーンを切り替える
@@ -14,6 +15,22 @@ namespace SceneAPI {
 	template<IsScene T, typename... Args>
 	void Change(Args&&... args) {
 		SceneManager::LoadScene(std::make_unique<T>(std::forward<Args>(args)...));
+	}
+
+	/// @brief シーンを切り替える(フェード処理有)
+	/// @tparam ...Args 切り替えるシーンの引数の型
+	/// @tparam T 切り替えるシーンの型
+	/// @param ...args 切り替えるシーンの引数
+	template<IsScene T, typename... Args>
+	void ChangeWithTransition(Args&&... args) {
+		if (TransitionManager::IsPlaying()) {
+			return;
+		}
+
+		TransitionManager::Play(
+			[args...]() {
+			SceneManager::LoadScene(std::make_unique<T>(std::forward<Args>(args)...));
+			});
 	}
 
 	/// @brief 現在のシーン名を取得する
